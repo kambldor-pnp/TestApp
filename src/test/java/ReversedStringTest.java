15ab5b67eafb93a739a2e4a38f47a71c85d7688a
Elzat Nurbek kyzy <elzatnurbekkyzy@Elzats-Air.hsd1.il.comcast.net>
2024-09-23 20:13:19 -0500

    first commit

---
+++ src/test/java/ReversedStringTest.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReversedStringTest {
+
+
+        @Test
+        public void reverseTestWithNull(){
+            Assert.assertNull(ReverseString.reverseString(null));
+            System.out.println("passed");
+        }
+
+        @Test
+        public void reverseTestWithDigits(){
+            Assert.assertEquals("cba321", ReverseString.reverseString("123abc"));
+            System.out.println("passed");
+        }
+
+        @Test
+        public void reverseTestHappyPath(){
+            Assert.assertEquals("elppa", ReverseString.reverseString("apple"));
+            System.out.println("passed");
+            System.out.println("Added new version");
+            System.out.println("Yeeahoo");
+        }
+
+    }
+
+
+
+
+
